75c12febaacf6daf8d33f4f9809bdc5d6ddfd277
1E10 Laptop <sample@bths.edu>
2022-09-30 12:28:18 -0400

    2 commit

---
+++ src/BoxRunner.java
@@ -0,0 +1,16 @@
+public class BoxRunner {
+    public static void main(String[] args) {
+//        Box newBox = new Box(6, 10, 8);
+//
+//
+//
+//
+//        boolean x = newBox.anySideLongerThan(5); // C
+//
+//
+//
+//        newBox.printDimensions(); // F
+//
+//        double volume = newBox.volume(21.8); // G
+    }
+}
